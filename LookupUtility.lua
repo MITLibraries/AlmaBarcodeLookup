f00aae9bc54a6e655dfaed9a3c204747eb042776
Adam Shire <shirea@mit.edu>
2022-05-05 16:02:41 -0400

    removes unnecessary semicolons

--- LookupUtility.lua
+++ LookupUtility.lua
@@ -1,29 +1,29 @@
-LookupUtility = {};
+LookupUtility = {}
 
 -- Load the .Net types that we will be using.
-local types = {};
-types["log4net.LogManager"] = luanet.import_type("log4net.LogManager");
-local log = types["log4net.LogManager"].GetLogger(rootLogger .. ".LookupUtility");
+local types = {}
+types["log4net.LogManager"] = luanet.import_type("log4net.LogManager")
+local log = types["log4net.LogManager"].GetLogger(rootLogger .. ".LookupUtility")
 
 local function GetValueDestination(fieldMapping, requestType)
-  local valueDestination = "";
+  local valueDestination = ""
   -- If the fieldMapping.ImportField is a string, it is a direct mapping from response to data field
   if(type(fieldMapping.ImportField) == "string") then
-    valueDestination = Utility.StringSplit(".", fieldMapping.ImportField);
+    valueDestination = Utility.StringSplit(".", fieldMapping.ImportField)
   else
       -- If it is not a string, it is a specific request type or an invalid fieldMapping
       if requestType == "Loan" then
-        log:Debug("requestType is Loan");
+        log:Debug("requestType is Loan")
         valueDestination = Utility.StringSplit(".", fieldMapping.ImportField.RequestType.Loan);
       elseif requestType == "Article" then
-        log:Debug("requestType is Article");
+        log:Debug("requestType is Article")
         valueDestination = Utility.StringSplit(".", fieldMapping.ImportField.RequestType.Article);
       else
-         log:Warn("Invalid request type");
+         log:Warn("Invalid request type")
       end
   end
-  return valueDestination;
+  return valueDestination
 end
 
 -- Exports
-LookupUtility.GetValueDestination = GetValueDestination;
+LookupUtility.GetValueDestination = GetValueDestination
